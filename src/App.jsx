e2a29e6ddcea3cf52f1777a64fea30cc6cc9fe8a
Ricky <ricky@example.com>
2026-02-15 21:28:34 +0100

    Fix: Add React Router to App.jsx - fixes white screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 import VendorDetail from './pages/VendorDetail'
 
 function App() {
-  return <VendorDetail />
+  return (
+    <BrowserRouter basename="/weddingfinder">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vendor/:slug" element={<VendorDetail />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
